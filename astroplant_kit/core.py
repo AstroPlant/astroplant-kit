b2c177fe1834159bb2fb40c2319e8c3c568c1a72
Beskhue <thomas@kepow.org>
2017-11-15 14:47:52 +0100

    Remove unused code.

--- astroplant_kit/core.py
+++ astroplant_kit/core.py
@@ -3,9 +3,6 @@ from kit import Kit
 import config
 
 if __name__ == "__main__":
-    #sensor_manager = SensorManager()
-    #sensor_manager.add_sensor(astroplant_sensor_library.sensors.mock.Mock)
-
     conf = config.read_config()
     api_client = astroplant_client.Client(conf["api"]["root"], conf["websockets"]["url"])
     api_client.authenticate(conf["auth"]["serial"], conf["auth"]["secret"])
