61384c7ca48f5574708c5adfcd5a7c6449ca7b81
AstroGeeks <astrogeeksgent@outlook.be>
2018-01-05 16:16:20 +0100

    Import Client class directly.

--- astroplant_kit/core.py
+++ astroplant_kit/core.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/..'))
 
 import logging
-import astroplant_client
+from astroplant_client import Client
 from astroplant_kit.kit import Kit
 from astroplant_kit import config
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         sys.exit(e.errno)
     
     logger.info('Creating AstroPlant network client.')
-    api_client = astroplant_client.Client(conf["api"]["root"], conf["websockets"]["url"])
+    api_client = Client(conf["api"]["root"], conf["websockets"]["url"])
 
     logger.info('Authenticating AstroPlant network client.')
     api_client.authenticate(conf["auth"]["serial"], conf["auth"]["secret"])
