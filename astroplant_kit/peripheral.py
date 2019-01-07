bcdc95f0320579121af772c8624256bcd66b718a
Thomas Churchman <thomas@kepow.org>
2019-01-07 20:59:04 +0100

    Make fewer measurements. Reduce over longer time periods.

--- astroplant_kit/peripheral.py
+++ astroplant_kit/peripheral.py
@@ -133,10 +133,10 @@ class Sensor(Peripheral):
     RUNNABLE = True
 
     #: Amount of time in seconds to wait between making measurements
-    TIME_SLEEP_BETWEEN_MEASUREMENTS = 0.5
+    TIME_SLEEP_BETWEEN_MEASUREMENTS = 2.0
 
     #: Amount of time in seconds over which measurements are reduced before publishing them for storage
-    TIME_REDUCE_MEASUREMENTS = 120.0
+    TIME_REDUCE_MEASUREMENTS = 1800.0
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
