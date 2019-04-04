fb27cbda2f23a57110542a8835d8561c51b6993f
Thomas Churchman <thomas@kepow.org>
2019-04-04 09:36:20 +0200

    Fix outdated comment.

--- astroplant_kit/config.py
+++ astroplant_kit/config.py
@@ -2,9 +2,9 @@ import toml
 
 def read_config():
     """
-    Read the configuration file at './kit_config.json'.
+    Read the configuration file at './kit_config.toml'.
 
-    :return: The json configuration as a dictionary.
+    :return: The TOML configuration as a dictionary.
     """
     with open('./kit_config.toml') as f:
         data = toml.load(f)
