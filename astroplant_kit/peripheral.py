AstroPlant/astroplant-kit

commit 628c60337c4c22e729f4921c6c3c7b80794f81dd

Update LocalDataLogger to new async API.

astroplant_kit/peripheral.py
@@ -629,25 +629,39 @@ class LocalDataLogger(Actuator):
     A virtual peripheral device writing observations to internal storage.
     """
 
+    RUNNABLE = True
+
     def __init__(self, *args, configuration):
         super().__init__(*args)
         self.storage_path = configuration["storagePath"]
 
-        # Subscribe to all aggregate measurements.
-        self.manager.subscribe_predicate(
-            lambda m: m.aggregate_type is not None, self._store_measurement
-        )
+    async def run(self):
+        """
+        Listen to new measurements and store them.
+        """
+        async for m in self.manager.measurements_receiver():
+            if m.aggregate_type is not None:
+                self._store_measurement(m)
 
     def _store_measurement(self, measurement):
         # Import required modules.
         import csv
         import os
 
-        measurement_dict = measurement.__dict__
+        measurement_dict = {
+            "datetime_start": measurement.datetime_start,
+            "datetime_end": measurement.datetime_end,
+            "peripheral": measurement.peripheral.get_id(),
+            "peripheral_name": measurement.peripheral.name,
+            "aggregate_type": measurement.aggregate_type,
+            "physical_quantity": measurement.quantity_type.physical_quantity,
+            "physical_unit": measurement.quantity_type.physical_unit,
+            "value": measurement.value,
+        }
 
         file_name = "%s-%s.csv" % (
             measurement.end_datetime.strftime("%Y%m%d"),
-            measurement.physical_quantity,
+            measurement.quantity_type.physical_quantity,
         )
         path = os.path.join(self.storage_path, file_name)
 

commit 76691bae64096f0485acbeb3f0d5a2f4c0d7070c

Fix typo introduced in 628c60337c.

astroplant_kit/peripheral.py
@@ -649,8 +649,8 @@ class LocalDataLogger(Actuator):
         import os
 
         measurement_dict = {
-            "datetime_start": measurement.datetime_start,
-            "datetime_end": measurement.datetime_end,
+            "start_datetime": measurement.start_datetime,
+            "end_datetime": measurement.end_datetime,
             "peripheral": measurement.peripheral.get_id(),
             "peripheral_name": measurement.peripheral.name,
             "aggregate_type": measurement.aggregate_type,