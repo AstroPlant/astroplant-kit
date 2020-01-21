6016db59914f98dd048d5033bbcbb54d130846a5
Thomas Churchman <thomas@kepow.org>
2020-01-22 00:30:10 +0100

    Create cache directory if it does not exist yet.

--- astroplant_kit/cache.py
+++ astroplant_kit/cache.py
@@ -8,6 +8,8 @@ QUANTITY_TYPES_CACHE = Path("quantity-types.json")
 class Cache(object):
     def __init__(self, cache_dir):
         self._dir = Path(cache_dir)
+        # Create cache directory if it does not exist yet.
+        self._dir.mkdir(parents=True, exist_ok=True)
 
     def write_configuration(self, configuration):
         with open(self._dir / CONFIGURATION_CACHE, "w") as f:
