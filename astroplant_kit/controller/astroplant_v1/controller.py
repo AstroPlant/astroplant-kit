c3434970b43eb73ebf97d6f988c3f1659e78de36
Thomas Churchman <thomas@kepow.org>
2020-07-08 04:12:04 +0200

    Implement scheduled outputs
    And handle cases of long-running or sometimes blocking peripheral
    commands better

--- astroplant_kit/controller/astroplant_v1/controller.py
+++ astroplant_kit/controller/astroplant_v1/controller.py
@@ -1,9 +1,10 @@
 import logging
 import trio
 from datetime import datetime, time
+from collections import defaultdict
 
 from enum import Enum
-from typing import NewType, Optional, Iterable, Callable, Dict, List, Set, Tuple
+from typing import NewType, Any, Optional, Iterable, Callable, Dict, List, Set, Tuple
 from typing_extensions import TypedDict
 
 from ...peripheral import PeripheralManager, Measurement
@@ -27,6 +28,7 @@ logger = logging.getLogger("astroplant_kit.controller.astroplant_v1")
 
 InputFuzzyVar = str
 OutputFuzzyVar = str
+OutputScheduleId = int
 TimeStr = str
 PeripheralName = str
 QuantityTypeIdStr = str
@@ -83,19 +85,29 @@ class OutputSettingsContinuous(TypedDict):
     maximal: float
 
 
-class OutputSettingsDiscrete(TypedDict):
-    values: List[float]
+class OutputSettingsScheduleEntry(TypedDict):
+    time: str
+    value: Any
+
+
+class OutputSettingsSchedule(TypedDict):
+    schedule: List[OutputSettingsScheduleEntry]
+
+
+class OutputSettingsScheduled(TypedDict):
+    interpolated: bool
+    schedules: List[OutputSettingsSchedule]
 
 
 class OutputTypes(Enum):
-    Discrete = "discrete"
+    Scheduled = "scheduled"
     Continuous = "continuous"
 
 
 class OutputSettings(TypedDict):
     type: OutputTypes
     continuous: Optional[OutputSettingsContinuous]
-    discrete: Optional[OutputSettingsDiscrete]
+    scheduled: Optional[OutputSettingsScheduled]
 
 
 class FuzzyRuleCondition(TypedDict):
@@ -113,9 +125,16 @@ class FuzzyRuleImplication(TypedDict):
     fuzzyVariable: OutputFuzzyVar
 
 
+class FuzzyRuleSchedule(TypedDict):
+    peripheral: PeripheralName
+    command: CommandName
+    schedule: OutputScheduleId
+
+
 class FuzzyRule(TypedDict):
     condition: List[FuzzyRuleCondition]  # Conjunction
     implication: List[FuzzyRuleImplication]  # Conjunction
+    schedules: List[FuzzyRuleSchedule]
     activeFrom: TimeStr
     activeTo: TimeStr
 
@@ -157,11 +176,12 @@ class Setpoints:
     def for_time(self, t: time) -> float:
         current_time = self._times[-1]
         next_time = self._times[0]
-        time_diff = seconds_between_times(current_time, next_time)
         for (idx, setpoint_time) in enumerate(self._times):
             if t >= setpoint_time:
                 current_time = setpoint_time
                 next_time = self._times[(idx + 1) % len(self._times)]
+
+        time_diff = seconds_between_times(current_time, next_time)
 
         current_setpoint = self._setpoints[current_time]
         next_setpoint = self._setpoints[next_time]
@@ -267,24 +287,65 @@ class Input:
         return self._membership_values[(peripheral, quantity_type, fuzzy_var)]
 
 
+class OutputSchedule:
+    def __init__(self, interpolated: bool, schedule: OutputSettingsSchedule):
+        self._schedule: Dict[time, Any] = {}
+        self._times: List[time] = []
+        self._interpolated = interpolated
+
+        for schedule_entry in schedule["schedule"]:
+            t = time.fromisoformat(schedule_entry["time"])
+            self._times.append(t)
+            self._schedule[t] = schedule_entry["value"]
+
+    def for_time(self, t: time) -> float:
+        current_time = self._times[-1]
+        next_time = self._times[0]
+        for (idx, schedule_entry_time) in enumerate(self._times):
+            if t >= schedule_entry_time:
+                current_time = schedule_entry_time
+                next_time = self._times[(idx + 1) % len(self._times)]
+
+        entry_time_diff = seconds_between_times(current_time, next_time)
+        time_elapsed_from_current = seconds_between_times(current_time, t)
+
+        current_schedule_entry = self._schedule[current_time]
+        next_schedule_entry = self._schedule[next_time]
+        if (
+            self._interpolated
+            and current_schedule_entry is not None
+            and next_schedule_entry is not None
+        ):
+            current_schedule_entry += (
+                (next_schedule_entry - current_schedule_entry)
+                / entry_time_diff
+                * time_elapsed_from_current
+            )
+        return current_schedule_entry
+
+
 class Output:
     def __init__(
         self, output_settings: Dict[PeripheralName, Dict[CommandName, OutputSettings]],
     ):
         self._shapes: Dict[OutputFuzzySet, Shape] = {}
-        self._output_transforms: Dict[
+        self._continuous_output_transforms: Dict[
             Tuple[PeripheralName, CommandName], Callable[[Fuzzy], float]
         ] = {}
-        self._membership_values: Dict[
+        self._scheduled_output_schedules: Dict[
+            Tuple[PeripheralName, CommandName], List[OutputSchedule]
+        ] = {}
+        self._continuous_membership_values: Dict[
             Tuple[PeripheralName, CommandName], Dict[OutputFuzzySet, Optional[Fuzzy]]
         ] = {}
+        self._scheduled_membership_values: Dict[
+            Tuple[PeripheralName, CommandName], Dict[OutputScheduleId, Optional[Fuzzy]]
+        ] = {}
 
-        self.triangle_half_width = 1.0 / 5
+        triangle_half_width = 1.0 / 5
         for (n, fuzzy_var) in enumerate(OutputFuzzySet):
             triangle_offset = 1.0 * n / (len(OutputFuzzySet) - 1)
-            self._shapes[fuzzy_var] = Triangle(
-                triangle_offset, self.triangle_half_width
-            )
+            self._shapes[fuzzy_var] = Triangle(triangle_offset, triangle_half_width)
 
         def output_transform(
             output_settings: OutputSettings,
@@ -303,39 +364,109 @@ class Output:
 
         for (peripheral, command_settings) in output_settings.items():
             for (command, settings) in command_settings.items():
-                self._membership_values[(peripheral, command)] = {}
-                for fuzzy_var in OutputFuzzySet:
-                    self._membership_values[(peripheral, command)][fuzzy_var] = None
-
                 type = OutputTypes(settings["type"])
-                if type is OutputTypes.Discrete:
-                    raise NotImplementedError(
-                        "Not yet implemented for discrete outputs"
-                    )
-                elif type is OutputTypes.Continuous:
-                    self._output_transforms[(peripheral, command)] = output_transform(
-                        settings
-                    )
+                if type is OutputTypes.Scheduled:
+                    self._scheduled_membership_values[(peripheral, command)] = {}
+                    self._scheduled_output_schedules[(peripheral, command)] = []
 
-    def set_membership_value(
+                    assert settings["scheduled"] is not None
+                    for (idx, schedule) in enumerate(
+                        settings["scheduled"]["schedules"]
+                    ):
+                        self._scheduled_membership_values[(peripheral, command)][
+                            idx + 1
+                        ] = None
+                        self._scheduled_output_schedules[(peripheral, command)].append(
+                            OutputSchedule(
+                                settings["scheduled"]["interpolated"], schedule
+                            )
+                        )
+                elif type is OutputTypes.Continuous:
+                    self._continuous_membership_values[(peripheral, command)] = {}
+                    for fuzzy_var in OutputFuzzySet:
+                        self._continuous_membership_values[(peripheral, command)][
+                            fuzzy_var
+                        ] = None
+
+                    self._continuous_output_transforms[
+                        (peripheral, command)
+                    ] = output_transform(settings)
+
+    @property
+    def scheduled_commands(self) -> Iterable[Tuple[PeripheralName, CommandName]]:
+        return self._scheduled_membership_values.keys()
+
+    def get_continuous_membership_value(
+        self,
+        peripheral: PeripheralName,
+        command: CommandName,
+        fuzzy_var: OutputFuzzySet,
+    ) -> Optional[Fuzzy]:
+        return self._continuous_membership_values[(peripheral, command)][fuzzy_var]
+
+    def set_continuous_membership_value(
         self,
         peripheral: PeripheralName,
         command: CommandName,
         fuzzy_var: OutputFuzzySet,
         value: Fuzzy,
     ) -> None:
-        self._membership_values[(peripheral, command)][fuzzy_var] = value
+        self._continuous_membership_values[(peripheral, command)][fuzzy_var] = value
 
-    def defuzzified_action(
+    def get_scheduled_membership_value(
+        self,
+        peripheral: PeripheralName,
+        command: CommandName,
+        schedule: OutputScheduleId,
+    ) -> Optional[Fuzzy]:
+        return self._scheduled_membership_values[(peripheral, command)][schedule]
+
+    def set_scheduled_membership_value(
+        self,
+        peripheral: PeripheralName,
+        command: CommandName,
+        schedule: OutputScheduleId,
+        value: Fuzzy,
+    ) -> None:
+        self._scheduled_membership_values[(peripheral, command)][schedule] = value
+
+    def _defuzzified_continuous_action(
         self, peripheral: PeripheralName, command: CommandName
     ) -> float:
-        fuzzy_values = self._membership_values[(peripheral, command)]
+        fuzzy_values = self._continuous_membership_values[(peripheral, command)]
         curves: List[Tuple[Shape, Fuzzy]] = [
             (self._shapes[fuzzy_var], value)
             for (fuzzy_var, value) in fuzzy_values.items()
             if value is not None
         ]
-        return self._output_transforms[(peripheral, command)](centroid(curves))
+        return self._continuous_output_transforms[(peripheral, command)](
+            centroid(curves)
+        )
+
+    def _defuzzified_scheduled_action(
+        self, peripheral: PeripheralName, command: CommandName, for_time: time
+    ) -> Any:
+        schedule_fuzzy_values = self._scheduled_membership_values[(peripheral, command)]
+
+        max_fuzzy = None
+        argmax_schedule_id = 1
+        for (schedule_id, fuzzy) in schedule_fuzzy_values.items():
+            if max_fuzzy is None or fuzzy is not None and fuzzy > max_fuzzy:
+                max_fuzzy = fuzzy
+                argmax_schedule_id = schedule_id
+
+        schedule = self._scheduled_output_schedules[(peripheral, command)][
+            argmax_schedule_id - 1
+        ]
+        return schedule.for_time(for_time)
+
+    def defuzzified_action(
+        self, peripheral: PeripheralName, command: CommandName, for_time: time
+    ) -> Any:
+        if (peripheral, command) in self._continuous_membership_values:
+            return self._defuzzified_continuous_action(peripheral, command)
+        elif (peripheral, command) in self._scheduled_membership_values:
+            return self._defuzzified_scheduled_action(peripheral, command, for_time)
 
 
 class Evaluator:
@@ -374,11 +505,17 @@ class Evaluator:
 
     class Rule:
         def __init__(self, fuzzy_rule: FuzzyRule):
+            self.truth = FUZZY_FALSE
+
             self.condition = [Evaluator.Condition(c) for c in fuzzy_rule["condition"]]
 
             self.implication = [
                 (i["peripheral"], i["command"], OutputFuzzySet(i["fuzzyVariable"]))
                 for i in fuzzy_rule["implication"]
+            ]
+            self.schedules = [
+                (i["peripheral"], i["command"], int(i["schedule"]))
+                for i in fuzzy_rule["schedules"]
             ]
 
             self.active_from = time.fromisoformat(fuzzy_rule["activeFrom"])
@@ -392,9 +529,19 @@ class Evaluator:
         self._output = output
 
     def process(
-        self, changed_inputs: Set[Tuple[PeripheralName, QuantityTypeId, InputFuzzySet]]
-    ) -> Set[Tuple[PeripheralName, CommandName, OutputFuzzySet]]:
-        output: Dict[Tuple[PeripheralName, CommandName, OutputFuzzySet], Fuzzy] = {}
+        self,
+        changed_inputs: Set[Tuple[PeripheralName, QuantityTypeId, InputFuzzySet]],
+        now: time,
+    ) -> Set[Tuple[PeripheralName, CommandName]]:
+        affected_commands = set()
+
+        total_implication: Dict[
+            Tuple[PeripheralName, CommandName, OutputFuzzySet], Fuzzy
+        ] = defaultdict(lambda: FUZZY_FALSE)
+        total_schedules: Dict[
+            Tuple[PeripheralName, CommandName, OutputScheduleId], Fuzzy
+        ] = defaultdict(lambda: FUZZY_FALSE)
+
         for rule in self._rules:
             evaluate = any(
                 map(
@@ -407,17 +554,35 @@ class Evaluator:
                 fuzzy = FUZZY_TRUE
                 for condition in rule.condition:
                     fuzzy = And(fuzzy, condition.evaluate(self._input))
+                rule.truth = fuzzy
 
+            if rule.active_from <= now <= rule.active_to:
                 for (p, command, fuzzy_var) in rule.implication:
-                    if (p, command, fuzzy_var) not in output:
-                        output[(p, command, fuzzy_var)] = FUZZY_FALSE
-                    output[(p, command, fuzzy_var)] = Or(
-                        output[(p, command, fuzzy_var)], fuzzy
+                    total_implication[(p, command, fuzzy_var)] = Or(
+                        total_implication[(p, command, fuzzy_var)], rule.truth
                     )
-        for ((p, command, fuzzy_var), truth) in output.items():
-            self._output.set_membership_value(p, command, fuzzy_var, truth)
 
-        return set(output.keys())
+                for (p, command, schedule) in rule.schedules:
+                    total_schedules[(p, command, schedule)] = Or(
+                        total_schedules[(p, command, schedule)], rule.truth
+                    )
+
+        # Currently, if due to the rules' from_time and to_time settings there are no
+        # longer active rules for a specific peripheral and command (but there have been
+        # previosuly), the last truth value stays active.
+        for ((p, command, fuzzy_var), truth) in total_implication.items():
+            old = self._output.get_continuous_membership_value(p, command, fuzzy_var)
+            if old != truth:
+                affected_commands.add((p, command))
+            self._output.set_continuous_membership_value(p, command, fuzzy_var, truth)
+
+        for ((p, command, schedule), truth) in total_schedules.items():
+            old = self._output.get_scheduled_membership_value(p, command, schedule)
+            if old != truth:
+                affected_commands.add((p, command))
+            self._output.set_scheduled_membership_value(p, command, schedule, truth)
+
+        return affected_commands
 
 
 class AstroplantControllerV1(Controller):
@@ -436,39 +601,61 @@ class AstroplantControllerV1(Controller):
         self._output = Output(fuzzy_control["output"])
         self._evaluator = Evaluator(fuzzy_control["rules"], self._input, self._output)
 
+        self._sending: Set[Tuple[PeripheralName, CommandName]] = set()
+        self._current_command_value: Dict[Tuple[PeripheralName, CommandName], Any] = {}
+
+    async def _do_command(self, peripheral_name: PeripheralName, command: CommandName):
+        """
+        Waits until the peripheral is ready to accept a command, then sends the most
+        recent command value.
+        """
+        if (peripheral_name, command) in self._sending:
+            return
+        self._sending.add((peripheral_name, command))
+
+        peripheral = self.peripheral_manager.get_peripheral_by_name(peripheral_name)
+        if peripheral is None:
+            logger.warning("the peripheral %s does not exist", peripheral_name)
+            return
+
+        async with self.peripheral_manager.control(peripheral) as do:
+            action = {command: self._current_command_value[(peripheral_name, command)]}
+            try:
+                logger.debug(
+                    "sending action to peripheral %s: %s", peripheral_name, action,
+                )
+                await do(action)
+            finally:
+                self._sending.remove((peripheral_name, command))
+
+    async def _perform_scheduled_commands(self) -> None:
+        scheduled_commands = list(self._output.scheduled_commands)
+
+        async with trio.open_nursery() as nursery:
+            while True:
+                t = datetime.now().time()
+                for (peripheral, command) in scheduled_commands:
+                    value = self._output.defuzzified_action(peripheral, command, t)
+                    if value != self._current_command_value.get((peripheral, command)):
+                        self._current_command_value[(peripheral, command)] = value
+                        nursery.start_soon(self._do_command, peripheral, command)
+                await trio.sleep(1.0)
+
     async def run(self) -> None:
         """
         TODO: apply rules on a timer as well, especially useful when
         measurements come in slowly.
         """
-        async for measurement in self.peripheral_manager.measurement_receiver():
-            changed = self._input.update(measurement)
-            commands_affected = self._evaluator.process(changed)
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(self._perform_scheduled_commands)
 
-            for (peripheral_name, command, _) in commands_affected:
-                peripheral = self.peripheral_manager.get_peripheral_by_name(
-                    peripheral_name
-                )
-                if peripheral is None:
-                    logger.warning("the peripheral %s does not exist", peripheral_name)
-                    continue
-                control = self.peripheral_manager.control(peripheral)
-                try:
-                    do = control.acquire_nowait()
-                    value = self._output.defuzzified_action(peripheral_name, command)
-                    action = {command: value}
-                    try:
-                        logger.debug(
-                            "sending action to peripheral %s: %s",
-                            peripheral_name,
-                            action,
-                        )
-                        await do(action)
-                    finally:
-                        control.release()
-                except trio.WouldBlock:
-                    # TODO: perhaps retry action after some time.
-                    logger.debug(
-                        "tried to control peripheral %s but could not immediately acquire lock",
-                        peripheral_name,
-                    )
+            async for measurement in self.peripheral_manager.measurement_receiver():
+                t = datetime.now().time()
+                changed = self._input.update(measurement)
+                commands_affected = self._evaluator.process(changed, t)
+
+                for (peripheral, command) in commands_affected:
+                    value = self._output.defuzzified_action(peripheral, command, t)
+                    if value != self._current_command_value.get((peripheral, command)):
+                        self._current_command_value[(peripheral, command)] = value
+                        nursery.start_soon(self._do_command, peripheral, command)
